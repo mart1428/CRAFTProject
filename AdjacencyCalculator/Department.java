7b1f58ae7dc3944ac8e0802025efc339ef445133
nerradfour <65404740+nerradfour@users.noreply.github.com>
2020-08-23 18:52:37 +0700

    Add files via upload

---
+++ AdjacencyCalculator/Department.java
@@ -0,0 +1,12 @@
+package src;
+
+public class Department {
+	 public String Name;
+     public int Index;
+
+     public Department(String name, int index)
+     {
+         Name = name;
+         Index = index;
+     }
+}
